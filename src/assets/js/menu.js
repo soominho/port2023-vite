a9f7bc74e147110abb4575e616dea90d2d8955a2
홍수민 <ghdtnals1@soominui-MacBookPro.local>
2023-11-28 18:44:33 +0900

    23.11.28-02

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -1,3 +1,14 @@
 export function menu() {
-    console.log(menu);
+    const headerToggle = document.getElementById("headerToggle");
+    const headerNav = document.querySelector(".header__nav");
+
+    headerToggle.addEventListener("click", () => {
+        headerNav.classList.toggle("show");
+
+        if(headerToggle) {
+            headerToggle.getAttribute("aria-expanded") === "true" 
+            ? headerToggle.setAttribute("aria-expanded", "false") 
+            : headerToggle.setAttribute("aria-expanded", "true");
+        }
+    });
 }
